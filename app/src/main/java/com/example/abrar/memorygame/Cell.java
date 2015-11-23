ad252adae6755ba69fcff310b10b328daa98a126
Abrar Al-Obaidi <eng.baro@gmail.com>
2015-11-23 21:02:46 +0000

    Flip card animation done
    I used the viewFlipper to do the animation and I started adding toolbar.

--- app/src/main/java/com/example/abrar/memorygame/Cell.java
+++ app/src/main/java/com/example/abrar/memorygame/Cell.java
@@ -1,8 +1,10 @@
 package com.example.abrar.memorygame;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 /**
  * Created by abrar on 26/10/15.
@@ -10,18 +12,30 @@ import android.widget.ImageView;
 public class Cell extends ImageView {
     private int mResId;
 
+
+
+
     public Cell(Context context, int resId) {
         super(context);
+
+
+       // front=(ImageView)findViewById(R.id.front);
         this.mResId = resId;
-        close();
+
+     //   close();
+        //the new way is to use inflater instead of the close
+
 
     }
 
     public void open() {
         setBackgroundResource(this.mResId);
+
+
     }
 
     public void close() {
-        setBackgroundResource(R.drawable.closed_cell);
+        //setBackgroundResource(R.drawable.closed_cell);
+
     }
 }
--- app/src/main/java/com/example/abrar/memorygame/MainActivity.java
+++ app/src/main/java/com/example/abrar/memorygame/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.abrar.memorygame;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -28,62 +36,43 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    //    objects.add(R.drawable.cat);
-      //  objects.add(R.drawable.cat);
-      //  objects.add(R.drawable.lobster);
-     //   objects.add(R.drawable.lobster);
-     //   objects.add(R.drawable.octopus);
-      //  objects.add(R.drawable.octopus);
-    //    objects.add(R.drawable.lion);
-      //  objects.add(R.drawable.lion);
-     //   objects.add(R.drawable.elephent);
-      //  objects.add(R.drawable.elephent);
-   //     Collections.shuffle(objects);
-
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
 
-   //     gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-     //       @Override
-       //     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-     //           Drawable s = new Drawable() {
-       //             @Override
-         //           public void draw(Canvas canvas) {
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-//                    }
 
-           /*         @Override
-                    public void setAlpha(int alpha) {
-
-                    }
-
-                    @Override
-                    public void setColorFilter(ColorFilter cf) {
-
-                    }
-
-                    @Override
-                    public int getOpacity() {
-                        return 0;
-                    }
-                };
-
-                view.setBackgroundResource(objects.get(i));
-                if (view.getBackground()==s)
-                    parent.setEnabled(false);
-                i++;
-                if (i>objects.size()+1){
-                    i=0;
-                    View m=view;
-                     s=m.getBackground();
-                }
 
             }
         });
-    */
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_item, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_refresh:
+                Toast.makeText(this, "Refresh selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            // action with ID action_settings was selected
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            default:
+                break;
+        }
 
-
+        return true;
+    }
 
 }
